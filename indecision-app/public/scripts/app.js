d895ef4da2f6208b904eb1a74e36e26ead373a17
asteriasandippal <sandip.pal@asteria.co.in>
2018-05-09 14:26:37 +0530

    Added Node package & Added gitignore file
     - adds React & Env babel preset
     - adds .gitignore file
     - adds package.json file
     - modifies documents.txt file
     - adds src folder

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,7 +1,12 @@
+"use strict";
+
 console.log("App.js is running!");
 
-// const template = <p>This is JSX From app.js!</p>;
-var template = React.createElement("h2", null, "This is JSX From app.js!");
-const appRoot = document.getElementById("app");
+var template = React.createElement(
+  "h1",
+  null,
+  "This is JSX From app.js!"
+);
+var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
---
+++ indecision-app/src/app.js
@@ -0,0 +1,6 @@
+console.log("App.js is running!");
+
+const template = <h1>This is JSX From app.js!</h1>;
+const appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
