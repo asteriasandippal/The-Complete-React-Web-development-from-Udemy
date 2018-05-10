fb6985d0bb3b4de4391594bf5dd2697179c9017d
asteriasandippal <sandip.pal@asteria.co.in>
2018-05-10 15:22:36 +0530

    Added es6 let and const

--- indecision-app/src/playground/es6-let-const.js
+++ indecision-app/src/playground/es6-let-const.js
@@ -1,14 +1,12 @@
-"use strict";
-
 var nameVar = "Sandip";
 var nameVar = "Rayel";
 console.log('nameVar', nameVar);
 
-var nameLet = "Jane";
+let nameLet = "Jane";
 nameLet = "Julei";
 console.log('nameLet', nameLet);
 
-var nameConst = "Frank";
+const nameConst = "Frank";
 // const nameConst = "Guther";
 console.log('nameConst', nameConst);
 
