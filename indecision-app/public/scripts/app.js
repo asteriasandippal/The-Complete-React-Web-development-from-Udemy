cc077d57bc090ed5eed36d328af6d73e44eae0c2
asteriasandippal <sandip.pal@asteria.co.in>
2018-05-10 13:06:13 +0530

    Modifies jsx Expresion & modifies app.js

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,48 +1,62 @@
 'use strict';
 
 console.log("App.js is running!");
-
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two', 'Three']
+};
 var template = React.createElement(
     'div',
     null,
     React.createElement(
         'h1',
         null,
-        'This is JSX From app.js!'
+        app.title
     ),
-    React.createElement(
+    app.subtitle && React.createElement(
         'p',
         null,
-        'Some Content here.'
+        app.subtitle
+    ),
+    app.options && React.createElement(
+        'p',
+        null,
+        app.options.length > 0 ? 'Here are your options' : 'No Options'
     )
 );
 var user = {
     name: 'Sandip',
     age: 24,
-    location: 'bangalore'
+    location: 'Bangalore'
 };
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    }
+}
 var templateTwo = React.createElement(
     'div',
     null,
     React.createElement(
         'h1',
         null,
-        user.name
+        user.name ? user.name : 'Anonymous'
     ),
-    React.createElement(
+    user.age && user.age >= 18 && React.createElement(
         'p',
         null,
         'Age: ',
         user.age
     ),
-    React.createElement(
-        'p',
-        null,
-        'Location: ',
-        user.location
-    )
+    getLocation(user.location)
 );
 
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,24 +1,34 @@
 console.log("App.js is running!");
-
+let app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two', 'Three']
+}
 const template = (
     <div>
-        <h1>This is JSX From app.js!</h1>
-        <p>Some Content here.</p>
+        <h1>{app.title}</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        {app.options && <p>{app.options.length > 0 ? 'Here are your options' : 'No Options'}</p>}
     </div>
 );
 let user = {
-    name: 'Sandip',
+    name: 'Sandip', 
     age: 24,
-    location: 'bangalore'
+    location: 'Bangalore'
+}
+function getLocation(location) {
+    if (location) {
+        return <p>Location: {location}</p>;
+    }
 }
 const templateTwo = (
     <div>
-        <h1>{user.name}</h1>
-        <p>Age: {user.age}</p>
-        <p>Location: {user.location}</p>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
     </div>
 );
 
 const appRoot = document.getElementById("app");
 
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);
