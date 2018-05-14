0162abf2160fea55d585349d16e0e75a5b86d1ec
asteriasandippal <sandip.pal@asteria.co.in>
2018-05-14 15:59:44 +0530

    Added component props

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,10 +1,13 @@
 class IndecisionApp extends React.Component {
     render() {
+        const title= "Indecision";
+        const subTitle = "Put your life in the hands of a computer";
+        const options = ['Thing One', 'Thing two', 'Thing three'];
         return (
             <div>
-                <Header />
+                <Header title={title} subTitle={subTitle}/>
                 <Action />
-                <Options />
+                <Options options={options} />
                 <AddOptions />
             </div>
         );
@@ -12,11 +15,12 @@ class IndecisionApp extends React.Component {
 }
 
 class Header extends React.Component {
+    
     render() {
         return (
             <div>
-                <h2>Indecision</h2>
-                <p>Put your life in the hands of a computer</p>
+                <h1>{this.props.title}</h1>
+                <h2>{this.props.subTitle}</h2>
             </div>
         );
     }
@@ -36,8 +40,7 @@ class Options extends React.Component {
     render() {
         return (
             <div>
-                Options component here
-                <Option />
+                {this.props.options.map((option) => <Option key={option} optionText={option}/>)}
             </div>
         );
     }
@@ -47,7 +50,7 @@ class Option extends React.Component {
     render() {
         return (
             <div>
-                Option component here
+               {this.props.optionText}
             </div>
         );
     }
