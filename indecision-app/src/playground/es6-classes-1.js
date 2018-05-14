2633e1717aa508c03bfa4793c8cf69d19b3f73d3
asteriasandippal <sandip.pal@asteria.co.in>
2018-05-14 14:22:51 +0530

    AddedEs6 Constructor class

--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -1,7 +1,5 @@
 class Person {
     constructor(name = 'Anonymous', age = 0) {
-        console.log(name);
-        console.log('Constructor');
         this.name = name;
         this.age = age;
     }
@@ -16,12 +14,53 @@ class Person {
     }
 }
 
-const me = new Person('Sandip Pal', 26);
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+
+    hasMajor() {
+        return !!this.major;
+    }
+    getDescription() {
+        let description = super.getDescription();
+        if (this.hasMajor()) {
+            description += ` Their major is ${this.major}.`;
+        }
+        return description;
+    }
+}
+
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+    getGretting() {
+        let gretting = super.getGretting();
+        if (this.homeLocation) {
+            gretting += ` I am visiting from ${this.homeLocation}.`;
+        }
+        return gretting;
+    }
+}
+
+const me = new Student('Sandip Pal', 26, 'Computer Science');
 console.log(me);
-console.log(me.getGretting());
+console.log(me.hasMajor());
 console.log(me.getDescription());
 
-const other = new Person();
+const other = new Student();
 console.log(other);
-console.log(other.getGretting());
-console.log(other.getDescription());
+console.log(other.hasMajor());
+console.log(me.getDescription());
+
+
+const me1 = new Traveler('Sandip Pal', 26, 'Philafrlphia');
+console.log(me1);
+console.log(me1.getGretting());
+
+const other1 = new Traveler();
+console.log(other1);
+console.log(other1.getGretting());
