a52baf3d4d25eaa2cbfdd4229f8de8c697c0eb67
asteriasandippal <sandip.pal@asteria.co.in>
2018-05-14 16:39:03 +0530

    Added Event & Method

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -27,19 +27,26 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+    handlePick() {
+        alert('hadlePick');
+    }
     render() {
         return (
             <div>
-                <button>What should I do?</button>
+                <button onClick={this.handlePick}>What should I do?</button>
             </div>
         );
     }
 }
 
 class Options extends React.Component {
+    handleRemoveAll() {
+        alert('handleRemoveAll');
+    }
     render() {
         return (
             <div>
+                <button onClick={this.handleRemoveAll}>Remove All</button>
                 {this.props.options.map((option) => <Option key={option} optionText={option}/>)}
             </div>
         );
@@ -57,10 +64,23 @@ class Option extends React.Component {
 }
 
 class AddOptions extends React.Component {
+    handleAddOption(e) {
+        e.preventDefault();
+
+        const option = e.target.elements.option.value.trim();
+        if (option) {
+            // this.options.push(option);
+            console.log(option);
+        }
+
+    }
     render() {
         return (
             <div>
-                AddOptions component here
+                <form onSubmit={this.handleAddOption}>
+                    <input type="text" name="option" />
+                    <button>Add Option</button>
+                </form>
             </div>
         );
     }
