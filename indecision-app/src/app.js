28eab332331ff1b8895424f2c2c25bf3bfa21eaa
asteriasandippal <sandip.pal@asteria.co.in>
2018-05-16 19:06:08 +0530

    Added css loader & style loader package

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import IndecisionApp from './components/IndecisionApp';
+import './styles/styles.css';
 
 ReactDOM.render(<IndecisionApp options={[]} />, document.getElementById('app'));
