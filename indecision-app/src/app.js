d0f9c7b908eba6a6ae46ddd6507f98ff75db201a
asteriasandippal <sandip.pal@asteria.co.in>
2018-05-16 12:28:17 +0530

    Added Es6 import/export

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,1 +1,12 @@
 console.log('Webpack runing...!');
+
+// import './utils.js';
+import { squer, add } from './utils';
+import { isAdult, canDrink } from './person';
+
+console.log(squer(4));
+console.log(add(23, 10));
+
+
+console.log(isAdult(18));
+console.log(canDrink(20));
---
+++ indecision-app/src/utils.js
@@ -0,0 +1,9 @@
+console.log('Utils js runing...!');
+
+const squer = (x) => x * x; 
+console.log(squer(3));
+
+const add = (x, y) => x + y; 
+console.log(add(3, 10));
+
+export {squer, add};
