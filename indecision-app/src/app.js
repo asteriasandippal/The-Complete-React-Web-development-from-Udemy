7e348a38142a96759d05f9b478a7fd9fb7e96ac6
asteriasandippal <sandip.pal@asteria.co.in>
2018-05-16 13:22:36 +0530

    Added validator package

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,13 +1,3 @@
-console.log('Webpack runing...!');
+import validator from 'validator';
 
-// import './utils.js';
-import substract, { squer, add } from './utils';
-import isSenior, { isAdult, canDrink } from './person';
-
-console.log(squer(4));
-console.log(add(23, 10));
-console.log(substract(292, 10));
-
-console.log(isAdult(18));
-console.log(canDrink(20));
-console.log(isSenior(65));
+console.log(validator.isEmail('test@fm.com'));
