2fd71ed93f5c6c946546a29768a30eb0a97d8353
asteriasandippal <sandip.pal@asteria.co.in>
2018-05-16 14:48:45 +0530

    Setting up babel and webpack

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const template = React.createElement('p', {}, 'Testing 123!');
+const template = <h2>Hello, I am</h2>;
 
 ReactDOM.render(template, document.getElementById('app'));
