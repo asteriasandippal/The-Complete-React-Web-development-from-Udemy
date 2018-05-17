31b8871fe7cf301ef3b8aa6516009e4cae353c42
asteriasandippal <sandip.pal@asteria.co.in>
2018-05-17 12:13:01 +0530

    Big Button & Options list

--- indecision-app/src/components/Option.js
+++ indecision-app/src/components/Option.js
@@ -3,9 +3,12 @@ import React from 'react';
 const Option = (props) => (
     <div>
         {props.optionText}
-        <button onClick={(e) => {
-            props.handleDeleteOption(props.optionText)
-        }}>
+        <button
+            className="button button--link"
+            onClick={(e) => {
+                props.handleDeleteOption(props.optionText)
+            }}
+        >
             remove
         </button>
     </div>
