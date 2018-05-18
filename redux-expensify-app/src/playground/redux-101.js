668fb42f282ac1311f5940d2e4a9ba7a4e1d2217
asteriasandippal <sandip.pal@asteria.co.in>
2018-05-18 19:46:02 +0530

    Subcribeing and Dynamic Actions

--- redux-expensify-app/src/playground/redux-101.js
+++ redux-expensify-app/src/playground/redux-101.js
@@ -2,41 +2,63 @@ import {createStore} from 'redux';
 
 const store = createStore((state = {count: 0}, action) => {
     console.log('runing');
-
     switch(action.type) {
         case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? 
+            action.incrementBy : 1;
             return {
-                count: state.count + 1
+                count: state.count + incrementBy
             }
         case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy === 'number' ?
+                action.decrementBy : 1;
             return {
-                count: state.count - 1
+                count: state.count - decrementBy
+            }
+        case 'SET':
+            return {
+                count: action.count
             }
         case 'RESET':
-        return {
-            count: 0
-        }
+            return {
+                count: 0
+            }
         default:
             return state;
     }
 });
 
-console.log(store.getState());
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
+});
 
 store.dispatch ({
-    type: 'INCREMENT'
+    type: 'INCREMENT',
+    incrementBy: 5
 });
+
+// unsubscribe();
 
 store.dispatch ({
     type: 'INCREMENT'
 });
 
 store.dispatch({
-    type: 'RESET'
+    type: 'RESET',
+});
+
+store.dispatch ({
+    type: 'DECREMENT',
+    decrementBy: 10
 });
 
 store.dispatch ({
     type: 'DECREMENT'
 });
 
-console.log(store.getState());
+store.dispatch({
+    type: 'SET',
+    count: 101
+});
+
+// console.log(store.getState());
