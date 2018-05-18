74bfaf076b8cdc0c800f78ded94494f0b6fd9409
asteriasandippal <sandip.pal@asteria.co.in>
2018-05-18 12:45:15 +0530

    Organizing  our routes

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,55 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch, Link, NavLink  } from 'react-router-dom';
 import 'normalize.css/normalize.css'; 
 import './styles/styles.scss';
 
-const ExpenseDashboardPage = () => (
-    <div>
-        This is from my dashboard
-    </div>
-);
+import AppRouter from './routers/AppRouter';
 
-const AddExpenseDashboardPage = () => (
-    <div>
-        This is from my Add dashboard
-    </div>
-);
-
-const EditExpenseDashboardPage = () => (
-    <div>
-        This is from my Edited dashboard
-    </div>
-);
-
-const NotFoundPage = () => (
-    <div>
-        404! <Link to="/">Go to Home</Link>
-    </div>
-);
-
-const Header = () => (
-    <header>
-        <h1>Expensify</h1>
-        <NavLink exact to="/" activeClassName="is-active">Dashboard</NavLink>
-        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-        <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
-        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
-    </header>
-);
-
-const routes = (
-    <BrowserRouter>
-        <div>
-            <Header/>
-            <Switch>
-                <Route path="/" component={ExpenseDashboardPage} exact />
-                <Route path="/create" component={AddExpenseDashboardPage}/>
-                <Route path="/edit" component={EditExpenseDashboardPage}/>
-                <Route component={NotFoundPage}/>
-            </Switch>
-        </div>
-    </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter/>, document.getElementById('app'));
---
+++ expensify-app/src/components/EditExpensePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EditExpensePage = () => (
+    <div>
+        This is from my Edited dashboard
+    </div>
+);
+
+export default EditExpensePage;
