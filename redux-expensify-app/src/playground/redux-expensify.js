afc4dc671f8a6513f3e4e4b97aa0ce3395823164
asteriasandippal <sandip.pal@asteria.co.in>
2018-05-21 18:36:45 +0530

    Added Spreading Objects & install package object rest spread

--- redux-expensify-app/src/playground/redux-expensify.js
+++ redux-expensify-app/src/playground/redux-expensify.js
@@ -24,6 +24,19 @@ const removeExpense = ({ id }) => ({
     id
 });
 
+
+const editExpense = (id, update) => ({
+    type: 'EDIT_EXPENSE',
+    id,
+    update
+
+});
+
+const setTextFilter = (text = '') => ({
+    type: 'SET_TEXT_FILTER',
+    text
+});
+
 // EXPENSES REDUCERS
 const expensesReducerDefaultState = [];
 
@@ -36,7 +49,18 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
                 action.expense
             ]
         case 'REMOVE_EXPENSE':
-            return state.filter(({id}) => id !== action.id)
+            return state.filter(({id}) => id !== action.id);
+        case 'EDIT_EXPENSE':
+            return  state.map((expense) => {
+                if (expense.id === action.id) {
+                    return {
+                        ...expense,
+                        ...action.update
+                    }
+                } else {
+                    return expense;
+                }
+            })
         default:
             return state;
     }
@@ -51,6 +75,11 @@ const filterReducerDefaultState =  {
 
 const filterReducer = (state = filterReducerDefaultState, action) => {
     switch(action.type) {
+        case 'SET_TEXT_FILTER':
+        return {
+            ...state,
+            text: action.text
+        }
         default:
             return state;
     }
@@ -76,6 +105,11 @@ store.dispatch(removeExpense({id: expenseTwo.expense.id}));
 
 console.log(expenseTwo.expense.id);
 
+store.dispatch(editExpense(expenseThree.expense.id, {amount: 500}));
+
+store.dispatch(setTextFilter('rent'));
+store.dispatch(setTextFilter());
+
 const demoState = {
     expenses: [{
         id: 'Propasdf Asa',
@@ -85,10 +119,19 @@ const demoState = {
         createdAt: 0
     }],
     filters: {
-        test: 'rent',
+        text: 'rent',
         sortBy: 'amount',
         startDate: undefined,
         endDate: undefined
     }
 }
 
+const user = {
+    name: 'sandip',
+    age: 32
+}
+console.log({
+    ...user,
+    location: 'Kolkata',
+
+});
