21924d7815d05d56270c5657ac001dc1ed307c6e
asteriasandippal <sandip.pal@asteria.co.in>
2018-05-21 16:03:19 +0530

    setup multiple reducers

--- redux-expensify-app/src/playground/redux-expensify.js
+++ redux-expensify-app/src/playground/redux-expensify.js
@@ -1,6 +1,41 @@
 console.log('redux-expesify');
 import { createStore, combineReducers } from 'redux';
 
+
+// EXPENSES REDUCERS
+const expensesReducerDefaultState = [];
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+};
+
+const filterReducerDefaultState =  {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+}
+
+const filterReducer = (state = filterReducerDefaultState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
+
+// STORE CREATION
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducer,
+        filters: filterReducer
+    })
+);
+
+console.log(store.getState());
+
 const demoState = {
     expenses: [{
         id: 'Propasdf Asa',
