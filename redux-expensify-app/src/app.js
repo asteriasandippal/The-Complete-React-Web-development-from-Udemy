29f6b4f9d37ff1527bd1d57a39fe7dce7a7db485
asteriasandippal <sandip.pal@asteria.co.in>
2018-05-22 12:33:35 +0530

    Organizing Redux

--- redux-expensify-app/src/app.js
+++ redux-expensify-app/src/app.js
@@ -4,5 +4,24 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 import AppRouter from './routers/AppRouter';
+import configureStore from '../src/store/configureStore';
+
+import { addExpense } from '../src/actions/expenses';
+import { setTextFilter } from '../src/actions/filters';
+import getVisibleExpense from '../src/selectors/expenses';
+
+const store = configureStore();
+
+store.dispatch(addExpense({description: 'Water Bill'}));
+store.dispatch(addExpense({description: 'Gas Bill'}));
+
+store.dispatch(setTextFilter('bill'));
+
+console.log(store.getState());
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpense(state.expenses, state.filters);
+
+console.log(visibleExpenses);
 
 ReactDOM.render(<AppRouter/>, document.getElementById('app'));
