1283802507ece0dd6b3a1eda798b93825ccfee7d
asteriasandippal <sandip.pal@asteria.co.in>
2018-05-23 16:46:12 +0530

    Added wiring up Add Expense

--- redux-expensify-app/src/components/ExpenseForm.js
+++ redux-expensify-app/src/components/ExpenseForm.js
@@ -26,50 +26,73 @@ export default class ExpenseForm extends React.Component{
 
     onAmountChange = (e) => {
         const amount = e.target.value;
-        if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+        if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
             this.setState(() => ({ amount }));
         }
     }
 
     onDateChange = (createdAt) => {
-        this.setState(() => ({ createdAt }));
+        if (createdAt) {
+            this.setState(() => ({ createdAt }));
+        }
     }
     onFocusChange = ({ focused }) => {
         this.setState(() => ({ focused }));
     }
 
+    onSubmit = (e) => {
+        e.preventDefault();
+
+        if (!this.state.description || !this.state.amount) {
+            this.setState(() => ({ error: 'Please provide description and amount'}));
+            
+        } else {
+            this.setState(() => ({ error: ''}));
+            console.log('Submitted!');
+            this.props.onSubmit({ 
+                description: this.state.description,
+                note: this.state.note,
+                amount: parseFloat(this.state.amount, 10) * 100,
+                createdAt: this.state.createdAt.valueOf(),
+            });
+        }
+    }
+
     render() {
         return (
             <div>
-                <input
-                    type="text"
-                    placeholder="Description"
-                    autoFocus
-                    value={this.state.description}
-                    onChange={this.onDescriptionChange}
-                />
-                <input
-                    type="text"
-                    placeholder="Amount"
-                    value={this.state.amount}
-                    onChange={this.onAmountChange}
-                />
-                <SingleDatePicker
-                    date={this.state.createdAt}
-                    onDateChange={this.onDateChange}
-                    focused={this.state.focused}
-                    onFocusChange={this.onFocusChange}
-                    numberOfMonths={1}
-                    isOutsideRange={(day) => false}
-                />
-                <textarea
-                    cols= "50"
-                    placeholder="Add a note for your expense (Optional)"
-                    value={this.state.note}
-                    onChange={this.onNoteChange}
-                >
-                </textarea>
-                <button>Add Expense</button>
+                {this.state.error && <p>{this.state.error}</p>}
+                <form onSubmit={this.onSubmit}>
+                    <input
+                        type="text"
+                        placeholder="Description"
+                        autoFocus
+                        value={this.state.description}
+                        onChange={this.onDescriptionChange}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Amount"
+                        value={this.state.amount}
+                        onChange={this.onAmountChange}
+                    />
+                    <SingleDatePicker
+                        date={this.state.createdAt}
+                        onDateChange={this.onDateChange}
+                        focused={this.state.focused}
+                        onFocusChange={this.onFocusChange}
+                        numberOfMonths={1}
+                        isOutsideRange={(day) => false}
+                    />
+                    <textarea
+                        cols= "50"
+                        placeholder="Add a note for your expense (Optional)"
+                        value={this.state.note}
+                        onChange={this.onNoteChange}
+                    >
+                    </textarea>
+                    <button>Add Expense</button>
+                </form>
             </div>
         )
     }
