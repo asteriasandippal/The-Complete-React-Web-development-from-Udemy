87a217c964f2a4916298ec69dc6d044314fb3544
asteriasandippal <sandip.pal@asteria.co.in>
2018-05-25 14:09:47 +0530

    Added enzyme enzyme-apapter react-test-render raf & react addons shallow compre and setup enzyme file and setup jest.config.json file

--- redux-expensify-app/src/tests/components/Header.test.js
+++ redux-expensify-app/src/tests/components/Header.test.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
 import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
-    const renderer = new ReactShallowRenderer();
-    renderer.render(<Header />);
-    console.log(renderer.getRenderOutput());
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
+    // const renderer = new ReactShallowRenderer();
+    // renderer.render(<Header />);
+    // console.log(renderer.getRenderOutput());
+    // expect(renderer.getRenderOutput()).toMatchSnapshot();
+
+    const wrapper = shallow(<Header />);
+    // expect(wrapper.find('h1').text()).toBe('Portfolio');
+    expect(toJson(wrapper)).toMatchSnapshot();
+
 });
 
