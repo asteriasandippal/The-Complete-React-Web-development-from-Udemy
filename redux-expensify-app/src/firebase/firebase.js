fa390fa1b6a113e560c61940127775137f77a2e9
asteriasandippal <sandip.pal@asteria.co.in>
2018-06-05 18:44:58 +0530

    Adds notes array in firebase

--- redux-expensify-app/src/firebase/firebase.js
+++ redux-expensify-app/src/firebase/firebase.js
@@ -12,30 +12,51 @@ const config = {
 firebase.initializeApp(config);
 const dataBase = firebase.database();
 
-dataBase.ref().set({
-  name: 'Sandip Pal',
-  age: 42,
-  stressLevel: 6,
-  job: {
-    title: 'Software Developer',
-    company: 'Google'
-  },
-  isSingle: null,
-  location: {
-      city: 'Bangalore',
-      country: 'India'
-  }
-}).then(() => {
-  console.log('Data is saved');
-}).catch((error) => {
-  console.log('This failed', error);
+// const notes = [{
+//   id: '12',
+//   title: 'First Note!',
+//   body: 'This is my note'
+// }, {
+//   id: '432',
+//   title: 'Another Note!',
+//   body: 'This is my note'
+// }];
+
+// dataBase.ref('Notes').set(notes);
+
+dataBase.ref('Notes').push({
+  title: 'ToDo',
+  body: 'React Native, Angular, pYthon'
 });
 
-dataBase.ref().update({
-  stressLevel: 6,
-  'job/company': 'Asteria',
-  'location/city': 'Kolkata'
+dataBase.ref('Notes/-LEEvB_1QfW2QrgYGBbh').update({
+  body: 'Buy food'
 });
+
+// dataBase.ref().set({
+//   name: 'Sandip Pal',
+//   age: 42,
+//   stressLevel: 6,
+//   job: {
+//     title: 'Software Developer',
+//     company: 'Google'
+//   },
+//   isSingle: null,
+//   location: {
+//       city: 'Bangalore',
+//       country: 'India'
+//   }
+// }).then(() => {
+//   console.log('Data is saved');
+// }).catch((error) => {
+//   console.log('This failed', error);
+// });
+
+// dataBase.ref().update({
+//   stressLevel: 6,
+//   'job/company': 'Asteria',
+//   'location/city': 'Kolkata'
+// });
 
 // dataBase.ref('isSingle')
 //   .remove()
@@ -45,15 +66,15 @@ dataBase.ref().update({
 //     console.log('Did not remove data',  error);
 //   });
 
-dataBase.ref()
-  .once( 'value' )
-  .then((dataSnapshot) => {
-    const val = dataSnapshot.val();
-    console.log('Once', val);
-  });
+// dataBase.ref()
+//   .once( 'value' )
+//   .then((dataSnapshot) => {
+//     const val = dataSnapshot.val();
+//     console.log('Once', val);
+//   });
 
-  dataBase.ref().on('value', (dataSnapshot) => {
-    console.log(dataSnapshot.val());
-    const val = dataSnapshot.val();
-    console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
-  });
+//   dataBase.ref().on('value', (dataSnapshot) => {
+//     console.log(dataSnapshot.val());
+//     const val = dataSnapshot.val();
+//     console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+//   });
