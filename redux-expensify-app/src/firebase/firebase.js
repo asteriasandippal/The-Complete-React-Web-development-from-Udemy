3176e522bd9798281cef5e0d9d5f213fbd93deca
asteriasandippal <sandip.pal@asteria.co.in>
2018-06-06 19:24:26 +0530

    Adds Creating s Separate Test Database

--- redux-expensify-app/src/firebase/firebase.js
+++ redux-expensify-app/src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import firebase, { database } from "firebase";
 
 const config = {
-  apiKey: "AIzaSyBi1Ii4BBauBozyRAeW2Or8mTCtEIoxq4Q",
-  authDomain: "expense-app-9b014.firebaseapp.com",
-  databaseURL: "https://expense-app-9b014.firebaseio.com",
-  projectId: "expense-app-9b014",
-  storageBucket: "expense-app-9b014.appspot.com",
-  messagingSenderId: "109624405620"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
